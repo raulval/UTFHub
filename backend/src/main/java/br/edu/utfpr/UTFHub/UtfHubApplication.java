4828bad3be5d017e143e5e86ead1ba05987ce8ac
Raul Val <raulval@outlook.com>
2021-10-08 14:01:26 -0300

    backend start
    Adicionando a pasta de backend com Spring Boot 
    Módulos utilizados: Spring Web, Lombok, Spring Security, JPA e PostgreSQL

---
+++ backend/src/main/java/br/edu/utfpr/UTFHub/UtfHubApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.UTFHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtfHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtfHubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/edu/utfpr/UTFHub/UtfHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.UTFHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtfHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
