a10f0b26a022c8950cb37247a9ab2d9d4763300d
Raul Val <raulval@outlook.com>
2021-11-05 17:40:06 -0300

    Criado um Redirect para o usuário não ter acesso a tela home sem estar logado

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { useHistory } from "react-router";
+import { Redirect, useHistory } from "react-router";
 import Header from "../components/Header";
 import HeaderContainerLogo from "../components/HeaderContainerLogo";
 import Search from "../components/Search";
@@ -7,10 +7,11 @@ import "../styles/home.css";
 
 export function Home() {
   const history = useHistory();
-  const userNome = useSelector((state) => state.userNome);
+  const userLogado = useSelector((state) => state.usuarioLogado);
 
   return (
     <div id="page-home">
+      {userLogado === 0 ? <Redirect to="/" /> : null}
       <HeaderContainerLogo />
       <Header />
       <button id="btn-materias" onClick={() => history.push("/materias")}>
